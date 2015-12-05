754dae035b968da798e2bfaea11ce099b379bf59
igor <igortr333@gmail.com>
2015-12-05 11:30:55 +0800

    add dependencies

--- src/main/scala/com/test/core/streaming/Fake.scala
+++ src/main/scala/com/test/core/streaming/Fake.scala
@@ -1,11 +1,11 @@
 package com.test.core.streaming
 
-// import org.apache.log4j.Logger
-// import org.apache.spark.SparkConf
-// import org.apache.spark.SparkContext
-// import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
-// import org.apache.spark.streaming.Milliseconds
-// import org.apache.spark.streaming.StreamingContext
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+import org.apache.spark.streaming.Milliseconds
+import org.apache.spark.streaming.StreamingContext
 
 
 object Fake {
